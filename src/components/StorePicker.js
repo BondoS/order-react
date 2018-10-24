21a2ed5d0b6a72cfeda3894801473fce58bfb584
Abdelrahman Serag <abdo.serag90@gmail.com>
2018-10-25 01:33:13 +0200

    finishing video 12 from the tutorial

---
+++ src/components/StorePicker.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+import PropTypes from 'prop-types';
+
+
+
+class StorePicker extends React.Component {
+
+  // constructor(){
+  //   super();
+  //   this.StorePicker = this.StorePicker.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+
+    const storeId = this.storeInput.value;
+    this.props.history.push(`/store/${storeId}`);
+    // first grab the text from the box 
+    // second we're going to transition from / to /store/:storeId
+  }
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+        { /* here is a comment */}        
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name"
+         defaultValue={getFunName()}
+         ref={(input) => { this.storeInput = input}}
+         />
+        <button type="submit">Visit Store -></button>
+      </form>
+    )
+  }
+}
+
+StorePicker.contextTypes = {
+  router: PropTypes.object
+}
+
+export default StorePicker;
+
+// render(<StorePicker/>, document.querySelector('#main'));
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './css/style.css';
-import App from './App';
-import StorePicker from './StorePicker';
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={StorePicker} />
+        <Route exact path="/store/:storeId" component={App} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+ReactDOM.render(<Root />, document.getElementById('main'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
