bf9e3c307a510cc4e7f171d909c37ec3be92389e
Abdelrahman Serag <abdo.serag90@gmail.com>
2018-10-26 00:37:07 +0200

    Set state, add new form, bindng...

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
-
+import React, { Component } from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+  addFish(fish) {
+    // update our state
+    const fishes = { ...this.state.fishes };
+    // add our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes });
+  }
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh seafood Market"/>
+          <Header tagline="Fresh seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
